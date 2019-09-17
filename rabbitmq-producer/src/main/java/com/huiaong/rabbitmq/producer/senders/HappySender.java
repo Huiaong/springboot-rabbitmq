b63a54d4e0ea694458bb41045f8cfd6ab7915364
Huiaong <Huiaong@outlook.com>
2019-09-17 08:51:19 +0800

    fix overdue import

--- rabbitmq-producer/src/main/java/com/huiaong/rabbitmq/producer/senders/HappySender.java
+++ rabbitmq-producer/src/main/java/com/huiaong/rabbitmq/producer/senders/HappySender.java
@@ -5,8 +5,8 @@ import com.huiaong.normal.trade.mq.model.BrokerMessageLog;
 import com.huiaong.normal.trade.mq.service.BrokerMessageLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
